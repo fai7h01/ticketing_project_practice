87aa490e240d847267829c5d94694ad8149dab8f
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-14 17:08:03 +0400

    config

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.cydeo.config;
 
+import com.cydeo.service.SecurityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
+
+    private final SecurityService securityService;
+    private final AuthSuccessHandler successHandler;
+
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler successHandler) {
+        this.securityService = securityService;
+        this.successHandler = successHandler;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -20,9 +30,15 @@ public class SecurityConfig {
                             .anyRequest().authenticated();
                 })
                 .formLogin(formLogin -> formLogin.loginPage("/login")
-                        .defaultSuccessUrl("/welcome")
+//                        .defaultSuccessUrl("/welcome")
+                        .successHandler(successHandler)
                         .failureUrl("/login?error=true")
                         .permitAll())
+                .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login"))
+                .rememberMe(rememberMe -> rememberMe.tokenValiditySeconds(1200)
+                        .key("ticketing")
+                        .userDetailsService(securityService))
                 .build();
 
     }
